3bfaf03440060b9212d9078be671e8e0ebd8ec23
lxndc <lexuanduc.wrk@gmail.com>
2024-09-24 14:45:04 +0700

    first commit

---
+++ HW1/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        System.out.println("Le Xuan Duc\t 23020527\t K68I-IS\t lnxdc \t 23020527@vnu.edu.vn");
+            for(int i = 9; i > 0; i--) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\n Take one down, pass it around,");
+            }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
